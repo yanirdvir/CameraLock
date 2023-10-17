3d30cfdc1045c27b1c6910bd3d35be84152c04d5
yanirdvir <yanirdvir159159@gmail.com>
2023-10-17 03:06:31 +0300

    Play sound on success and make sure events won't change for next 5 seconds after event changed

--- events/event_handler.py
+++ events/event_handler.py
@@ -1,6 +1,53 @@
+from datetime import datetime
+from enum import Enum
+from pathlib import Path
+
+from playsound import playsound
+
+SECONDS_BETWEEN_DIFFERENT_EVENTS = 5
+
+
+class Event(Enum):
+    ON_SUCCESS = "on_success"
+    ON_FAILURE = "on_failure"
+    NO_EVENT = "no_event"
+
+
+last_event = Event.NO_EVENT
+last_event_changed_time = datetime.now()
+
+
+def play_success_sound() -> None:
+    print("Playing success sound...")
+
+    filename = "events/sounds/success_sound.mp3"
+    filename = Path(filename)
+    playsound(filename)
+
+
+def update_event_changed(new_event: Event) -> bool:
+    global last_event
+    global last_event_changed_time
+
+    if (
+        last_event != new_event
+        and (datetime.now() - last_event_changed_time).total_seconds() >= SECONDS_BETWEEN_DIFFERENT_EVENTS
+    ):
+        last_event = new_event
+        last_event_changed_time = datetime.now()
+        return True
+    return False
+
+
 def on_success() -> None:
     print("Success!")
+    if not update_event_changed(Event.ON_SUCCESS):
+        return
+
+    play_success_sound()
 
 
 def on_failure() -> None:
     print("Failure!")
+    if not update_event_changed(Event.ON_FAILURE):
+        return
